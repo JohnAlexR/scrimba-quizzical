2c764db6deee6c393bddd186e6b6f08f291d6e76
Bence Fekete <fekete965@gmail.com>
2023-07-12 19:29:37 +0100

    Run prettier on Question.js

--- src/Question.js
+++ src/Question.js
@@ -1,51 +1,59 @@
 import React from "react";
 import Answer from "./Answer";
 
-export default function Question({question, correct, incorrect, roundNumber, isSelected, isCorrect, selectAnswer}) {
+export default function Question({
+  question,
+  correct,
+  incorrect,
+  roundNumber,
+  isSelected,
+  isCorrect,
+  selectAnswer,
+}) {
+  const answerArray = [correct, incorrect[0], incorrect[1], incorrect[2]];
+  const [answerEl, setAnswerEl] = React.useState(() => {
+    console.log("answerEl state ran");
+    return [];
+  });
 
-    const answerArray = [correct,incorrect[0],incorrect[1],incorrect[2]]
-    const [answerEl, setAnswerEl] = React.useState(()=>{
-        console.log('answerEl state ran')
-        return []})
+  function shuffle(array) {
+    let currentIndex = array.length;
+    let randomIndex;
 
-    function shuffle(array) {
-        let currentIndex = array.length
-        let randomIndex
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
 
-        while(currentIndex != 0) {
-            randomIndex = Math.floor(Math.random()*currentIndex)
-            currentIndex--
-
-            [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
-            }
-            return array;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
     }
+    return array;
+  }
 
-    React.useEffect(()=>{    
-    
-    const shuffledArray = shuffle(answerArray)
-        
-    console.log('question use effect ran')
-    
-    setAnswerEl(()=>shuffledArray.map(answer=>(
-        <Answer 
-            key={answer}
-            answer={answer}
-            correct={correct}
-            select={()=>selectAnswer(answer)}
-            isCorrect={isCorrect}
+  React.useEffect(() => {
+    const shuffledArray = shuffle(answerArray);
+
+    console.log("question use effect ran");
+
+    setAnswerEl(() =>
+      shuffledArray.map((answer) => (
+        <Answer
+          key={answer}
+          answer={answer}
+          correct={correct}
+          select={() => selectAnswer(answer)}
+          isCorrect={isCorrect}
         />
-    )))},[roundNumber])
+      ))
+    );
+  }, [roundNumber]);
 
-
-    return (
-        <div className="question-container">
-             <h3>{question}</h3>
-             <div className="answer-container">
-                {answerEl}
-             </div>
-        </div>
-
-    )
+  return (
+    <div className="question-container">
+      <h3>{question}</h3>
+      <div className="answer-container">{answerEl}</div>
+    </div>
+  );
 }
